71901d64c83e2e0d570c134be5e0517f9b112866
Ibrahim Mansoor Khalid <ibrahimmkhalid@gmail.com>
2025-05-21 19:18:09 -0700

    cleanup

--- ibkbd-gamer/ergogon/chamfer.js
+++ ibkbd-gamer/ergogon/chamfer.js
@@ -1,27 +1,22 @@
 #!/usr/bin/env node
-const fs = require('fs');
+const fs = require("fs");
 const inpath = "./output/cases/top_plate.jscad";
-const outpath = "../3d-files/"
+const outpath = "../3d-files/";
 const screwtop_radius = 1.95;
 const screwtop_height = 0.8;
 const screw_radius = 1.1;
 
-const jscad_data = fs.readFileSync(inpath, 'utf8');
-const lines = jscad_data.split('\n');
+const jscad_data = fs.readFileSync(inpath, "utf8");
+const lines = jscad_data.split("\n");
 
 cylinders = [];
 
 lines.forEach((line) => {
   if (line.includes("CAG.circle")) {
-    let xy = line.match("center\":\\[(.*)\]")[1].split(",");
+    let xy = line.match('center":\\[(.*)\]')[1].split(",");
     let x = xy[0];
     let y = xy[1];
-    let cyl = "new CSG.cylinder({" +
-      "  start: [" + x + "," + y + ","+ (2 - screwtop_height) +"]," +
-      "  end: [" + x + "," + y + "," + 2 + "]," +
-      "  radiusStart: " + screw_radius + "," +
-      "  radiusEnd: " + screwtop_radius + "" +
-      "})";
+    let cyl = `new CSG.cylinder({start:[${x},${y},${2 - screwtop_height}],end:[${x},${y},2],radiusStart:${screw_radius},radiusEnd:${screwtop_radius}})`;
     cylinders.push(cyl);
   }
 });
@@ -44,8 +39,10 @@ lines.forEach((line) => {
   }
 });
 
-const outdata = newlines.join('\n');
+const outdata = newlines.join("\n");
 
 fs.writeFileSync(outpath + "top_plate.jscad", outdata);
-fs.copyFileSync("./output/cases/bottom_plate.jscad", outpath + "bottom_plate.jscad");
-
+fs.copyFileSync(
+  "./output/cases/bottom_plate.jscad",
+  outpath + "bottom_plate.jscad",
+);
